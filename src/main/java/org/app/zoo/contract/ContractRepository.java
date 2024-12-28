5ad722ebc7124cb66be2df02864255a5b1b70bd4
DaryllLorenzo <vikingwarrior4444@gmail.com>
2024-12-28 14:39:31 -0500

    Feat: Stats contractByProviderType

--- src/main/java/org/app/zoo/contract/ContractRepository.java
+++ src/main/java/org/app/zoo/contract/ContractRepository.java
@@ -1,9 +1,11 @@
 package org.app.zoo.contract;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -13,4 +15,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 // JpaSpecificationExecutor -> para realizar consultas con especificaciones
 public interface ContractRepository extends JpaRepository<Contract, Integer>, JpaSpecificationExecutor<Contract>{
     boolean existsByProviderIdAndStartingDateAndEndingDateAndConciliationDateAndDescriptionAndBasePrice(int providerId, Date startingDate, Date endingDate, Date concilitationDate, String description, double basePrice);
+
+    // obtener tipos de proveedores con mas contratos
+    @Query("SELECT pt.name, COUNT(c) " +
+           "FROM Contract c " +
+           "JOIN c.provider p " +
+           "JOIN p.providerType pt " +
+           "WHERE c.endingDate >= CURRENT_DATE AND c.startingDate <= CURRENT_DATE " +
+           "GROUP BY pt.name")
+    List<Object[]> countActiveContractsByProviderType();
 }
