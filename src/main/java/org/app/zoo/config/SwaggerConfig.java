2bd4baf2ca1938588760d4eb2df12fa6d1876afe
cStoneDev <carlosmiguelpiedraalvarez@gmail.com>
2025-01-13 18:43:42 -0500

    feat: Enhance Swagger configuration with alphabetical sorting for schemas and paths

--- src/main/java/org/app/zoo/config/SwaggerConfig.java
+++ src/main/java/org/app/zoo/config/SwaggerConfig.java
@@ -1,9 +1,15 @@
 package org.app.zoo.config;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.Components;
@@ -14,14 +20,34 @@ public class SwaggerConfig {
     @Bean
     OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info().title("API Title").version("1.0"))
-            .components(new Components()
-                .addSecuritySchemes("bearerAuth", 
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")))
-            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+                .info(new Info().title("Zoo API").version("1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
-}
 
+    @Bean
+    public OpenApiCustomizer sortSchemasAlphabetically() {
+        return openApi -> {
+            Map<String, Schema> schemas = openApi.getComponents().getSchemas();
+            openApi.getComponents().setSchemas(new TreeMap<>(schemas));
+        };
+    }
+    
+    @Bean
+    public OpenApiCustomizer sortPathsAlphabetically() {
+        return openApi -> {
+            if (openApi.getPaths() != null) {
+                // Ordenar las rutas alfabéticamente
+                Paths sortedPaths = new Paths();
+                new TreeMap<>(openApi.getPaths()).forEach(sortedPaths::addPathItem);
+                openApi.setPaths(sortedPaths);
+            }
+        };
+    }
+
+}
